4e479cc4eab0e30d1a1ba86b13c9baef9f94803b
ayozemt <ayozemt@gmail.com>
2023-12-11 14:08:37 +0100

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import foods from './foods.json';
 import { useState } from 'react';
+import FoodBox from './components/FoodBox';
+import { Row, Divider } from 'antd';
 
 function App() {
   const [food, setFoods] = useState(foods);
   return (
     <div className="App">
-      <h1>Iron Nutrition</h1>
-      {food.map((unit) => {
+      <h1 style={{ textAlign: 'center' }}>Iron Nutrition</h1>
+      {/* {food.map((unit) => {
         return (
           <div>
             <h3>Food list</h3>
@@ -15,7 +17,21 @@ function App() {
             <img src={unit.image} alt={unit.name} style={{ height: '150px' }} />
           </div>
         );
-      })}
+      })} */}
+      {/* <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      /> */}
+      <Divider>Food List</Divider>
+      <Row>
+        {food.map((unit, index) => {
+          return <FoodBox key={index} food={unit} />;
+        })}
+      </Row>
     </div>
   );
 }
